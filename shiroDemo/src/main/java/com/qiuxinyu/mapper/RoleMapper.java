bac6bffabcf5d26fb214c89ed7f46ed718b99766
qiuxinyu <1277544516@qq.com>
2022-10-16 21:14:59 +0800

    项目重构

--- shiroDemo/src/main/java/com/qiuxinyu/mapper/RoleMapper.java
+++ shiroDemo/src/main/java/com/qiuxinyu/mapper/RoleMapper.java
@@ -1,13 +1,9 @@
 package com.qiuxinyu.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.qiuxinyu.entity.Perm;
 import com.qiuxinyu.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 @Mapper
 public interface RoleMapper extends BaseMapper<Role> {
-//    List<Perm> getPermsByRole(String role);
 }
--- shiroDemo/src/main/java/com/qiuxinyu/mapper/UserMapper.java
+++ shiroDemo/src/main/java/com/qiuxinyu/mapper/UserMapper.java
@@ -5,9 +5,6 @@ import com.qiuxinyu.entity.Role;
 import com.qiuxinyu.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-//    List<Role> getRolesByUsername(String username);
 }
